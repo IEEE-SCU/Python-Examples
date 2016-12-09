23d9bf00fb02d72686bce9643f271818b0061917
D4Vinci <D4Vinci@users.noreply.github.com>
2016-12-09 17:04:09 +0200

    Update ClearSubtitle.py

--- ClearSubtitle.py
+++ ClearSubtitle.py
@@ -6,11 +6,13 @@ import sys,re
 srt = sys.argv[1]
 codes = ["</font>","<i>","</i>"]
 font_regex = re.compile(r".font color=.#[a-z|A-Z|0-9]*..") #"""<font color="#FFA500">"""
+font_regex2 = re.compile(r".font color=#[a-z|A-Z|0-9]*.")  #"""<font color=#FFA500>"""
 ra5ama_regex = re.compile(r"{.*}") #"""{\fs50\fad(1000,1500)\c&#008008&\..\...\..\....}"""
 f=open(srt,"r")
 subtitles=f.read()
 f.close()
 extracted=font_regex.findall(subtitles)
+extracted+=font_regex2.findall(subtitles)
 extracted+=ra5ama_regex.findall(subtitles)
 all = extracted + codes
 for i in all :
